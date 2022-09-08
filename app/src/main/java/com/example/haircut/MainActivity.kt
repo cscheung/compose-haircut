b935a1a5a1e503831d7baec95cbdaacbf1a03ebc
Crystal Cheung <crystal.cheung@partners.goodrx.com>
2022-09-07 22:34:12 -0700

    Evenly spaced cards in grid

--- app/src/main/java/com/example/haircut/MainActivity.kt
+++ app/src/main/java/com/example/haircut/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.example.haircut
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.haircut.ui.theme.HaircutTheme
 import com.example.haircut.Service
 
@@ -41,12 +44,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Layout(services: ArrayList<Service>) {
-    LazyVerticalGrid(columns = GridCells.Fixed(3)) {
-        items(
-            items = services
-            ) { service ->
-            GridCellContents(service = service)
-        }
+    Scaffold(
+    ) {
+        ServicesGrid(services = services)
     }
 }
 
@@ -62,39 +62,54 @@ fun OfferCard() {
     )
 }
 
-
-/*
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun VerticalGrid(services: ArrayList<Service>) {
+fun ServicesGrid(services: ArrayList<Service>) {
+    val spaceBetweenItems = 10.dp
     LazyVerticalGrid(
-        cells = GridCells.Fixed(3),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalArrangement = Arrangement.SpaceEvenly
+        modifier = Modifier.padding(horizontal = spaceBetweenItems, vertical = spaceBetweenItems),
+        columns = GridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(spaceBetweenItems),
+        verticalArrangement = Arrangement.spacedBy(spaceBetweenItems)
     ) {
-        items(services) { service ->
+        items(
+            items = services
+        ) { service ->
             GridCellContents(service = service)
         }
     }
 }
 
- */
-
 @Composable
 fun GridCellContents(service: Service) {
-    Column(
+    Card(
         modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly
+            .fillMaxSize()
+            .background(Color.Green),
+        shape = RoundedCornerShape(25)
     ) {
-        Icon(
-            painter = painterResource(id = service.icon),
-            contentDescription = "grid_content_icon"
-        )
-        Text(
-            text = service.name
-        )
+        Box(
+            modifier = Modifier
+                .aspectRatio(1f)
+        ) {
+            Column(
+                modifier = Modifier
+                    .background(Color.Cyan)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly,
+            ) {
+
+                Icon(
+                    modifier = Modifier
+                        .background(Color.Blue),
+                    painter = painterResource(id = service.icon),
+                    contentDescription = "grid_content_icon"
+                )
+                Text(
+                    text = service.name
+                )
+            }
+        }
     }
 
 }
