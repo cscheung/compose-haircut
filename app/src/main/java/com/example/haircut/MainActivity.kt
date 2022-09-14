1223b96b8f3c6323ea00eb06639ad5c45d0cf70c
Crystal Cheung <crystal.cheung@partners.goodrx.com>
2022-09-13 18:13:37 -0700

    Add barbers images row

--- app/src/main/java/com/example/haircut/MainActivity.kt
+++ app/src/main/java/com/example/haircut/MainActivity.kt
@@ -9,15 +9,18 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.haircut.ui.theme.HaircutTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,6 +53,53 @@ fun MainScreen(services: ArrayList<Service>) {
 }
 
 @Composable
+fun OurBarbersCard() {
+    val imageUrl = "https://picsum.photos/200"
+    val imageSize = 45.dp
+    val rowPadding = 20.dp
+    val imagePadding = 20.dp
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        content = {
+            Row(
+                modifier = Modifier.padding(rowPadding),
+                horizontalArrangement = Arrangement.spacedBy(imagePadding)
+            ) {
+                AsyncImage(
+                    modifier = Modifier
+                        .size(imageSize)
+                        .clip(CircleShape),
+                    model = imageUrl,
+                    contentDescription = "barber"
+                )
+                AsyncImage(
+                    modifier = Modifier
+                        .size(imageSize)
+                        .clip(CircleShape),
+                    model = imageUrl,
+                    contentDescription = "barber"
+                )
+                AsyncImage(
+                    modifier = Modifier
+                        .size(imageSize)
+                        .clip(CircleShape),
+                    model = imageUrl,
+                    contentDescription = "barber"
+                )
+                AsyncImage(
+                    modifier = Modifier
+                        .size(imageSize)
+                        .clip(CircleShape),
+                    model = imageUrl,
+                    contentDescription = "barber"
+                )
+            }
+        }
+    )
+
+}
+
+@Composable
 fun OfferCard() {
     val padding = 20.dp
     Card(
@@ -74,6 +124,7 @@ fun ServicesGrid(services: ArrayList<Service>) {
         horizontalArrangement = Arrangement.spacedBy(spaceBetweenItems),
         verticalArrangement = Arrangement.spacedBy(spaceBetweenItems)
     ) {
+
         item(
             span = { GridItemSpan(2)}
         ) {
@@ -82,6 +133,11 @@ fun ServicesGrid(services: ArrayList<Service>) {
                     OfferCard()
                 }
             )
+        }
+        item (
+            span = { GridItemSpan(2) }
+        ) {
+            OurBarbersCard()
         }
         items(
             items = services
